7b819cd2ffec86ada8fd40b5c66441f21e25225a
Jose <jose.gr98@yahoo.com>
2025-06-19 13:54:35 -0300

    -Added questionable setup to test stuff

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -160,3 +162,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ],
 }
+
+
+# Allow all origins (development only!)
+CORS_ALLOW_ALL_ORIGINS = True
